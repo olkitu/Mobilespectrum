4743b91ec8848ca3dd3c0e4e21e3cd9652abdbef
Minh Truong <minh@truong.fi>
2021-01-16 22:24:16 +0200

    Add support more bands, add Edzcom Finland and other small design update

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ COPY nginx.conf /etc/nginx/nginx.conf
 
 EXPOSE 80
 
-HEALTHCHECK --interval=1m --start-period=5s --timeout=3s CMD curl -f http://localhost:80/ || exit 1
+HEALTHCHECK --interval=1m --timeout=3s CMD curl -f http://localhost:80/ || exit 1
--- src/app/spectrum-pages/spectrum-pages.component.ts
+++ src/app/spectrum-pages/spectrum-pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { type } from 'os';
 import { CommonServiceService, Frequencies } from '../common-service.service';
 
 export interface Bands {
@@ -45,11 +46,27 @@ export class SpectrumPagesComponent implements OnInit {
     {band: 5, type: 'FDD', name: '850 MHz'},
     {band: 7, type: 'FDD', name: '2600 MHz'},
     {band: 8, type: 'FDD', name: '900 MHz'},
+    {band: 12, type: 'FDD', name: '700 MHz'},
+    {band: 13, type: 'FDD', name: '700 MHz'},
+    {band: 14, type: 'FDD', name: '700 MHz'},
+    {band: 17, type: 'FDD', name: '700 MHz'},
+    {band: 18, type: 'FDD', name: '800 MHz'},
+    {band: 19, type: 'FDD', name: '900 MHz'},
     {band: 20, type: 'FDD', name: '800 MHz'},
+    {band: 25, type: 'FDD', name: '1900 MHz'},
+    {band: 26, type: 'FDD', name: '850 MHz'},
     {band: 28, type: 'FDD', name: '700 MHz'},
+    {band: 29, type: 'SDL', name: '700 MHz'},
+    {band: 31, type: 'FDD', name: '450 MHz'},
     {band: 32, type: 'SDL', name: '1500 MHz'},
     {band: 38, type: 'TDD', name: '2600 MHz'},
-    {band: 40, type: 'TDD', name: '2300 MHz'}
+    {band: 39, type: 'TDD', name: '1900 MHz'},
+    {band: 40, type: 'TDD', name: '2300 MHz'},
+    {band: 41, type: 'TDD', name: '2500 MHz'},
+    {band: 42, type: 'TDD', name: '3500 MHz'},
+    {band: 43, type: 'TDD', name: '3700 MHz'},
+    {band: 66, type: 'FDD', name: '1700/2100 MHz'},
+    {band: 71, type: 'FDD', name: '600 MHz'}
   ]
     
 
