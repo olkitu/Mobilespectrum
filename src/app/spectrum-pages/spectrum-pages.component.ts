ff49325a3af7c282f407587c15130b81b5dc240e
KenSoft <kken62@gmail.com>
2022-01-22 20:23:45 +0700

    Fix Minh's Bug

--- src/app/spectrum-pages/spectrum-pages.component.ts
+++ src/app/spectrum-pages/spectrum-pages.component.ts
@@ -84,9 +84,9 @@ export class SpectrumPagesComponent implements OnInit {
       if(this.countryCapitalized != null){
         this.countryCapitalized = this.countryCapitalized.split('_').join(' ');
         this.countryCapitalized = this.countryCapitalized.toLowerCase().replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
+        this.countryTitle = countryCodeEmoji(lookup.byCountry(this.countryCapitalized).iso2) + " " + this.countryCapitalized;
       }
       //console.log(this.countryCapitalized);
-      this.countryTitle = countryCodeEmoji(lookup.byCountry(this.countryCapitalized).iso2) + " " + this.countryCapitalized;
       this.region = params['region'];
       this.doGetCountryData(this.country, this.region);
       if (this.country != null) {
